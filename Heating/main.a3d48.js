bc48eadd69300cc4c30b649f1a2b198c1fca7919
Jeffrin JC <jeffrin.c@byjus.com>
2021-10-22 18:36:01 +0530

    Update

--- Heating/main.a3d48.js
+++ Heating/main.a3d48.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.0e6da.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.ae37a.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
